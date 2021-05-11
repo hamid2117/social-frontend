6537201a602987888a19b5c9ef00b41a0b0c9ab7
hamid2117 <saim008865@gmail.com>
2021-05-11 05:16:51 +0500

    added proxy in package-json and uri in apollo prvider

--- src/apolloPriovider.js
+++ src/apolloPriovider.js
@@ -6,7 +6,7 @@ import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { setContext } from 'apollo-link-context'
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://dry-ravine-43132.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
